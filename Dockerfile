a962ba744310f21070c7935aaf7df2847a2affc6
yuriji <dbfl9711@gmail.com>
2023-06-29 12:47:32 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM node:14.15.2
+
+ADD https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh wait-for-it.sh
+RUN chmod +x wait-for-it.sh
---
+++ setup.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/IoTKETI/Mobius.git
+
+cp ./src/app.js ./Mobius/app.js
+cp ./src/mobius.js ./Mobius/mobius.js
+
+cd Mobius
+
+sudo npm install
+
+cd ..
+
