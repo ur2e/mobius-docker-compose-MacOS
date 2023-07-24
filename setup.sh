21e9c3032f3a3afbc554530ea0cdf184636f8ebb
Yuri JI <45191069+ur2e@users.noreply.github.com>
2023-07-24 14:29:50 +0900

    Add: .env file check

--- setup.sh
+++ setup.sh
@@ -9,3 +9,10 @@ sudo npm install
 
 cd ..
 
+DB_CONFIG_FILE=".env"
+
+if [ ! -e $DB_CONFIG_FILE ]; then
+    echo "\n.env file not exists. Declare MY_PASSWORD variable in .env file."
+else
+    echo "\nsetup complete."
+fi
